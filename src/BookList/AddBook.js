8b99b0cfb0ae7c5f9dae9dbd4302d380f43209ef
Fiona <fionaJang@glaibo.com>
2021-04-27 11:51:33 +0800

    Add edit book function and check input function in both AddBook and EditBook

--- src/BookList/AddBook.js
+++ src/BookList/AddBook.js
@@ -8,21 +8,37 @@ const AddBook = () => {
     const [title, setTitle] = useState("")
     const [author, setAuthor] = useState("")
     const [desc, setDesc] = useState("")
+    const [alert, setAlert] = useState("")
 
     const handleClearInput = () => {
         setTitle("");
         setAuthor("");
         setDesc("");
+        setAlert("");
     }
 
     const handleAddBook = () => {
+        if(title === "") {
+            setAlert("請填寫名稱")
+            return
+        }
+        if(author === "") {
+            setAlert("請填寫作者")
+            return
+        }
         const addBookDetail = {
             title, author, description: desc
         }
-        addData(getBooksUrl, addBookDetail);
-        setTitle("");
-        setAuthor("");
-        setDesc("");
+        addData(getBooksUrl, addBookDetail)
+            .then(() => {
+                setTitle("");
+                setAuthor("");
+                setDesc("");
+                setAlert("");
+            })
+            .catch(() => {
+                console.log("新增失敗")
+            })
     }
 
     return (
@@ -39,6 +55,7 @@ const AddBook = () => {
                     <input type="text" placeholder="作者" value={author} onChange={(e) => setAuthor(e.target.value)}/>
                     <input type="text" placeholder="備註" value={desc} onChange={(e) => setDesc(e.target.value)}/>
                 </div>
+                <div>{alert}</div>
                 <div>
                     <button onClick={handleClearInput}>取消</button>
                     <button onClick={handleAddBook}>新增</button>
--- src/BookList/BookDetail.js
+++ src/BookList/BookDetail.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import EditBook from "./EditBook";
 import {Switch, Link, Route, useRouteMatch, useParams} from "react-router-dom";
+import {getData} from "../util";
+import {getBooksUrl} from "../constants";
 
 const BookDetail = () => {
     let {bookId} = useParams();
     let match = useRouteMatch();
+    const [bookDetail, setBookDetail] = useState('')
+
+    useEffect(() => {
+        getData(getBooksUrl + bookId).then((res) => {
+            setBookDetail(res.data)
+        })
+    }, [])
+
     return (
         <>
             <Switch>
-                <Route path={`${match.path}/edit`} component={EditBook}/>
+                <Route path={`${match.path}/edit`} render={(props) => <EditBook bookTitle={bookDetail.title} {...props} />} />
                 <Route path={match.path}>
-                    <button>
-                        <Link to="/books">back</Link>
-                    </button>
-                    <h3>Requested topic ID: {bookId}</h3>
-                    <Link to={`${match.url}/edit`}>edit</Link>
+                    <div>
+                        <button>
+                            <Link to="/books">back</Link>
+                        </button>
+                        <h1>{bookDetail.title}</h1>
+                        <Link to={`${match.url}/edit`}>edit</Link>
+                    </div>
+                    <div>
+                        <div>名稱：{bookDetail.title}</div>
+                        <div>作者：{bookDetail.author}</div>
+                        <div>備註：{bookDetail.description}</div>
+                    </div>
                 </Route>
             </Switch>
         </>
