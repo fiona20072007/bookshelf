e1e25e55c995032bbbedcf3ca5336764490d9a66
Fiona <fionaJang@glaibo.com>
2021-04-28 11:31:46 +0800

    Add lazy loading and loading component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
-import BookIndex from './BookIndex'
-import BookList from "./BookList"
-import AddBook from "./BookList/AddBook";
-import NoMatch from "./NoMatch"
+import React, {Suspense, lazy} from "react";
 import {Switch, Route} from "react-router-dom";
-import BookDetail from "./BookList/BookDetail";
 import styles from "./scss/App.module.scss";
+import Loading from "./Common/Loading";
+const BookIndex = lazy(() => import('./BookIndex'))
+const BookList = lazy(() => import('./BookList'))
+const AddBook = lazy(() => import('./BookList/AddBook'))
+const NoMatch = lazy(() => import('./NoMatch'))
+const BookDetail = lazy(() => import('./BookList/BookDetail'))
+
 
 function App() {
     return (
-        <Switch className={styles.app}>
-            <Route exact path="/" component={BookIndex}/>
-            <Route path="/books/:bookId" component={BookDetail}/>
-            <Route path="/books" component={BookList}/>
-            <Route path="/add" component={AddBook}/>
-            <Route path="*" component={NoMatch}/>
-        </Switch>
+        <Suspense fallback={<Loading />}>
+            <Switch className={styles.app}>
+                <Route exact path="/" component={BookIndex}/>
+                <Route path="/books/:bookId" component={BookDetail}/>
+                <Route path="/books" component={BookList}/>
+                <Route path="/add" component={AddBook}/>
+                <Route path="*" component={NoMatch}/>
+            </Switch>
+        </Suspense>
     );
 }
 
